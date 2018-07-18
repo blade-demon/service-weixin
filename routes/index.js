9c77640df4c215fdfa1d32aaa8e8027f8045c596
ziwei <ziwei@gamepoch.com>
2018-07-18 10:37:26 +0800

    remove unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var jsSHA = require('jssha');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', {title: 'Express'});
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Gamepoch' });
 });
 
 
@@ -18,30 +18,28 @@ router.get('/auth', (req, res) => {
   var token = "gamepoch*2018";
   var signature = req.query.signature;
   var timestamp = req.query.timestamp;
-  var echostr   = req.query.echostr;
-console.log("echostr:", echostr);
-  var nonce     = req.query.nonce;
+  var echostr = req.query.echostr;
+  console.log("echostr:", echostr);
+  var nonce = req.query.nonce;
   var oriArray = new Array();
   oriArray[0] = nonce;
   oriArray[1] = timestamp;
   oriArray[2] = token;
   oriArray.sort();
   var original = oriArray.join('');
- var shaObj = new jsSHA("SHA-1",'TEXT');
+  var shaObj = new jsSHA("SHA-1", 'TEXT');
   shaObj.update(original);
   var hash = shaObj.getHash("HEX");
   console.log(hash);
-  if(signature == hash){
+  if (signature == hash) {
     //验证成功
     return res.send(echostr);
-  } else {
+  }
+  else {
     //验证失败
     return res.status(500).send('ok');
   }
 });
 
-router.post('/auth', (req, res) => {
-
-});
 
 module.exports = router;
